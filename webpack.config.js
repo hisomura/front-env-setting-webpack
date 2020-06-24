abcb40c86030f0103e1162d1a6e4b665f5c1dd0c
hisomura <hisomura.noreply@gmail.com>
2020-06-25 07:18:46 +0900

    レクチャー7まで

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,43 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
   devtool: false,
-  entry: {app: './src/app.js'},
+  entry: {app: './src/app.js', sub: './src/sub.js'},
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           'sass-loader'
         ]
+      }, {
+        test: /\.(jpe?g|gif|png|svg|woff2?|ttf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[contenthash].[ext]',
+              outputPath: 'images',
+              publicPath: 'images'
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css'
+    })
+  ]
 }
