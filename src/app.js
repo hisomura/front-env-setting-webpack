d9609bf17ddb4bed0d9224d00f27bf31759e39ae
hisomura <hisomura.noreply@gmail.com>
2020-06-26 20:01:30 +0900

    レクチャー11まで

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@
 import './app.scss'
 
 const init = async () => {
-  await asyncFn();
+  await asyncFn()
   console.log("This is a main js file.")
+  jQuery()
+  g = 0
 }
 
 async function asyncFn() {
-  console.log('Im async function');
+  console.log('Im async function')
   console.log([1, 2, 3].includes(0))
 }
 
-init();
+init()
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
       },
       {
         test: /\.js$/,
